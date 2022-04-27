29d92d69ea94f82ccb246c4ae2d128d519b5c8a7
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2022-04-27 11:15:41 +0530

    value encoding add

--- src/utils/otpGenerator.ts
+++ src/utils/otpGenerator.ts
@@ -1,15 +1,16 @@
 import { ClassicLevel } from 'classic-level';
 
-const levelDB = new ClassicLevel('../../db');
+const levelDB = new ClassicLevel('../../db', {
+  valueEncoding: 'json',
+});
 
 const randomWords = require('random-words'); // TODO: Replace with a secure random number generator
 
 export class OTPGenerator {
   async generateOtp(user: string) {
     const otp = randomWords({ exactly: 3, join: '-' });
-    await levelDB.put(user, otp).then(() => {
-      return otp;
-    });
+    await levelDB.put(user, otp);
+    return otp;
   }
 
   async verifyOtp(otp: string, user: string): Promise<boolean | string> {
